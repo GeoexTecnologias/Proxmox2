672237057157d95c65d79e8ece4eccccc2cf5e31
Rudá Cunha <ruda@ruda.com.br>
2024-09-15 14:33:08 -0300

    Update install_k3s.sh

--- install_k3s.sh
+++ install_k3s.sh
@@ -1,39 +1,5 @@
 #!/bin/bash
 
-# Função para configurar hostname e rede
-configure_network() {
-    NEW_HOSTNAME=$1
-    IP_ADDRESS=$2
-    NETMASK=$3
-    GATEWAY=$4
-    DNS=$5
-
-    # Configura o hostname
-    echo "Configurando o hostname para $NEW_HOSTNAME..."
-    hostnamectl set-hostname $NEW_HOSTNAME
-    echo "127.0.0.1 $NEW_HOSTNAME" >> /etc/hosts
-
-    # Configura a interface de rede
-    echo "Configurando rede com IP: $IP_ADDRESS, Máscara: $NETMASK, Gateway: $GATEWAY, DNS: $DNS..."
-    NETPLAN_CONFIG="/etc/netplan/01-netcfg.yaml"
-    cat <<EOF > $NETPLAN_CONFIG
-network:
-  version: 2
-  ethernets:
-    ens18:  # Altere a interface se necessário (ex: ens192)
-      addresses:
-        - $IP_ADDRESS/$NETMASK
-      gateway4: $GATEWAY
-      nameservers:
-        addresses:
-          - $DNS
-EOF
-
-    # Aplica as configurações de rede
-    netplan apply
-    echo "Configuração de rede aplicada com sucesso."
-}
-
 # Função para instalar Docker
 install_docker() {
     echo "Instalando Docker..."
@@ -82,21 +48,15 @@ install_k3s() {
 
 # Função principal
 main() {
-    if [ "$#" -lt 6 ]; then
-        echo "Uso: $0 <primary|secondary> <NEW_HOSTNAME> <IP_ADDRESS> <NETMASK> <GATEWAY> <DNS> [ETCD_ENDPOINT] [TOKEN]"
+    if [ "$#" -lt 1 ]; then
+        echo "Uso: $0 <primary|secondary> [ETCD_ENDPOINT] [TOKEN]"
         exit 1
     fi
 
     NODE_TYPE=$1
-    NEW_HOSTNAME=$2
-    IP_ADDRESS=$3
-    NETMASK=$4
-    GATEWAY=$5
-    DNS=$6
-    ETCD_ENDPOINT=$7
-    TOKEN=$8
+    ETCD_ENDPOINT=$2
+    TOKEN=$3
 
-    configure_network $NEW_HOSTNAME $IP_ADDRESS $NETMASK $GATEWAY $DNS
     install_docker
     install_k3s $NODE_TYPE $ETCD_ENDPOINT $TOKEN
     install_portainer
