a0f3753c5c7d79447f636ca2f2868c74b1eaa730
Rudá Cunha <ruda@ruda.com.br>
2024-09-15 14:35:47 -0300

    Update install_k3s.sh

--- install_k3s.sh
+++ install_k3s.sh
@@ -3,22 +3,22 @@
 # Função para instalar Docker
 install_docker() {
     echo "Instalando Docker..."
-    apt update
-    apt install -y apt-transport-https ca-certificates curl software-properties-common
+    sudo apt update
+    sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
     curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
     echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-    apt update
-    apt install -y docker-ce docker-ce-cli containerd.io
-    systemctl start docker
-    systemctl enable docker
+    sudo apt update
+    sudo apt install -y docker-ce docker-ce-cli containerd.io
+    sudo systemctl start docker
+    sudo systemctl enable docker
     echo "Docker instalado com sucesso."
 }
 
 # Função para instalar o Portainer
 install_portainer() {
     echo "Instalando Portainer..."
-    docker volume create portainer_data
-    docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
+    sudo docker volume create portainer_data
+    sudo docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
     echo "Portainer instalado com sucesso."
 }
 
@@ -30,7 +30,7 @@ install_k3s() {
 
     if [ "$NODE_TYPE" == "primary" ]; then
         echo "Instalando K3s como nó primário..."
-        curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server --cluster-init --datastore-endpoint=etcd" sh -
+        curl -sfL https://get.k3s.io | sudo INSTALL_K3S_EXEC="server --cluster-init --datastore-endpoint=etcd" sh -
         echo "K3s nó primário instalado com sucesso."
     elif [ "$NODE_TYPE" == "secondary" ]; then
         if [ -z "$ETCD_ENDPOINT" ] || [ -z "$TOKEN" ]; then
@@ -38,7 +38,7 @@ install_k3s() {
             exit 1
         fi
         echo "Instalando K3s como nó secundário..."
-        curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server --server https://$ETCD_ENDPOINT:6443 --token $TOKEN" sh -
+        curl -sfL https://get.k3s.io | sudo INSTALL_K3S_EXEC="server --server https://$ETCD_ENDPOINT:6443 --token $TOKEN" sh -
         echo "K3s nó secundário instalado com sucesso."
     else
         echo "Tipo de nó inválido. Use 'primary' para o primeiro nó ou 'secondary' para adicionar um nó ao cluster."
