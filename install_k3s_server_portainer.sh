0942cb58d9f8e781817360d425a6d8f544144d1c
Rudá Cunha <ruda@ruda.com.br>
2024-09-16 15:59:32 -0300

    Update install_k3s_server_portainer.sh

--- install_k3s_server_portainer.sh
+++ install_k3s_server_portainer.sh
@@ -20,8 +20,8 @@ export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
 sleep 10
 
 # Adiciona taint ao nó controlador para impedir que outros pods sejam agendados, exceto o Portainer
-echo "Configurando o nó controlador para aceitar apenas o Portainer..."
-kubectl taint nodes $(hostname) dedicated=controller:NoSchedule
+#echo "Configurando o nó controlador para aceitar apenas o Portainer..."
+#kubectl taint nodes $(hostname) dedicated=controller:NoSchedule
 
 # Instala o Helm
 echo "Instalando o Helm..."
@@ -60,7 +60,7 @@ helm install portainer portainer/portainer -n portainer -f values.yaml
 
 # Adiciona a toleration manualmente ao deployment do Portainer
 echo "Adicionando a toleration ao deployment do Portainer..."
-kubectl patch deployment portainer -n portainer --type='json' -p='[{"op": "add", "path": "/spec/template/spec/tolerations", "value": [{"key":"dedicated","operator":"Equal","value":"controller","effect":"NoSchedule"}]}]'
+#kubectl patch deployment portainer -n portainer --type='json' -p='[{"op": "add", "path": "/spec/template/spec/tolerations", "value": [{"key":"dedicated","operator":"Equal","value":"controller","effect":"NoSchedule"}]}]'
 
 echo "Instalação concluída!"
 echo "K3s server com etcd e Portainer instalados no controlador k3s."
