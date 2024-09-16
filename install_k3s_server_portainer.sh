df8b438ab73534e00eeb7048883216dcb50d9063
Rudá Cunha <ruda@ruda.com.br>
2024-09-16 12:53:42 -0300

    Update install_k3s_server_portainer.sh

--- install_k3s_server_portainer.sh
+++ install_k3s_server_portainer.sh
@@ -10,8 +10,8 @@ echo "Iniciando a instalação do K3s com etcd no servidor controlador..."
 sudo apt update && sudo apt upgrade -y
 
 echo "Instalando o K3s com etcd como datastore..."
-# Instala o K3s com etcd como datastore e desabilita o agente
-curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server --disable-agent --datastore-endpoint='etcd'" sh -
+# Instala o K3s com etcd como datastore
+curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server --datastore-endpoint='etcd'" sh -
 
 # Exporte a variável KUBECONFIG para usar o kubectl
 export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
@@ -38,14 +38,16 @@ helm repo update
 echo "Criando o namespace para o Portainer..."
 kubectl create namespace portainer
 
-# Instala o Portainer via Helm com tolerância para rodar no nó controlador
+# Instala o Portainer via Helm com tolerância para rodar no nó controlador, sem expor serviços desnecessários
 echo "Instalando o Portainer no K3s via Helm..."
 helm install portainer portainer/portainer -n portainer \
   --set tolerations[0].key="dedicated" \
   --set tolerations[0].operator="Equal" \
   --set tolerations[0].value="controller" \
   --set tolerations[0].effect="NoSchedule" \
-  --set service.type=LoadBalancer
+  --set service.ports.http.enabled=false \
+  --set service.ports.https.enabled=true \
+  --set service.ports.https.port=9443
 
 echo "Instalação concluída!"
 echo "K3s server com etcd e Portainer instalados no controlador k3s."
