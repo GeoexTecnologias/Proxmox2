41fe37668c17dd8c673db2f85cb9375174f63cfe
Rudá Cunha <ruda@ruda.com.br>
2024-09-20 22:11:45 -0300

    Update install_k3s_server_portainer.sh

--- install_k3s_server_portainer.sh
+++ install_k3s_server_portainer.sh
@@ -1,52 +1,33 @@
 #!/bin/bash
 
-# Atualiza o timezone
-echo "Atualizando o timezone para America/Bahia"
-sudo timedatectl set-timezone America/Bahia
+echo "Atualizando o sistema..."
+apk update && apk upgrade
 
-echo "Iniciando a instalação do K3s com etcd no servidor controlador..."
+echo "Instalando dependências..."
+apk add curl iptables ip6tables socat
 
-# Atualiza o sistema
-sudo apt update && sudo apt upgrade -y
+echo "Instalando K3s com etcd como datastore..."
+curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server --cluster-init --datastore-endpoint=etcd" sh -
 
-echo "Instalando o K3s com etcd como datastore..."
-# Instala o K3s com etcd como datastore
-curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server --datastore-endpoint='etcd'" sh -
+echo "Verificando o status do K3s..."
+systemctl status k3s
 
-# Exporte a variável KUBECONFIG para usar o kubectl
-export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
+echo "Configurando K3s para iniciar automaticamente..."
+rc-update add k3s default
 
-# Aguarde o K3s inicializar
-sleep 10
+echo "Instalando Docker para o Portainer agent..."
+apk add docker
+service docker start
+rc-update add docker
 
-# Instala o Helm
-echo "Instalando o Helm..."
-curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
+echo "Instalando Portainer agent..."
+docker volume create portainer_data
+docker run -d \
+    -p 9001:9001 \
+    --name portainer_agent \
+    --restart=always \
+    -v /var/run/docker.sock:/var/run/docker.sock \
+    -v portainer_data:/data \
+    portainer/agent
 
-# Adiciona o repositório do Portainer no Helm
-echo "Adicionando o repositório do Portainer no Helm..."
-helm repo add portainer https://portainer.github.io/k8s/
-
-# Atualiza os repositórios do Helm
-helm repo update
-
-# Cria o namespace para o Portainer
-echo "Criando o namespace para o Portainer..."
-kubectl create namespace portainer
-
-# Instala o Portainer via Helm com tolerância para rodar no nó controlador, sem expor serviços desnecessários
-echo "Instalando o Portainer no K3s via Helm..."
-helm install portainer portainer/portainer -n portainer \
-  --set service.type=NodePort \
-  --set service.ports.http.enabled=false \
-  --set service.ports.https.enabled=true \
-  --set service.ports.https.port=9443 \
-  --set service.ports.https.nodePort=9443
-
-echo "Instalação concluída!"
-echo "K3s server com etcd e Portainer instalados no controlador k3s."
-echo "Token para conectar os nós ao controlador:"
-sudo cat /var/lib/rancher/k3s/server/node-token
-
-# Exibe o IP para conexão ao Portainer
-echo "Conecte-se ao Portainer em https://$(hostname -I | awk '{print $1}'):9443"
+echo "Instalação concluída no servidor."
